cc6c16d272e869535402774398d8a91666e4490e
Rithik Tank <2233293@slu.edu.ph>
2024-09-01 16:41:16 +0800

    Initial Commit

---
+++ cs311l-9365-swift/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoView.swift
+//  cs311l-9365-swift
+//
+//  Created by Rithik Tank on 9/1/24.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text("TODO LIST")
+    }
+}
+
+#Preview {
+    TodoView()
+}
