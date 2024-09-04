fe95a10ccb6ec5b5caf08c0307a979a222b61562
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-09-04 21:41:57 +0800

    Created course and todolist item struct / objects

---
+++ cs311l-9365-swift/classes/Course.swift
@@ -0,0 +1,8 @@
+//
+//  Course.swift
+//  cs311l-9365-swift
+//
+//  Created by Johan on 9/4/24.
+//
+
+import Foundation
---
+++ cs311l-9365-swift/classes/TodoItem.swift
@@ -0,0 +1,8 @@
+//
+//  TodoItem.swift
+//  cs311l-9365-swift
+//
+//  Created by Johan on 9/4/24.
+//
+
+import Foundation
