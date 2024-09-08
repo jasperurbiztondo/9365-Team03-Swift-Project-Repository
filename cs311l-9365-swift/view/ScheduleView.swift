2addc2c0de998c818506972ea394d7a68f5bb33d
Rithik Tank <2233293@slu.edu.ph>
2024-09-08 12:51:28 +0800

    fixed an exception

---
+++ cs311l-9365-swift/view/ScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  cs311l-9365-swift
+//
+//  Created by Rithik Tank on 9/8/24.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
