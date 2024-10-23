a573420333f7ba186ae731a55596ec11702cfd43
Karl Jasper Urbiztondo <2233067@slu.edu.ph>
2024-10-23 15:01:30 +0800

    +added framework +added new framework test for faster running time

---
+++ cs311l-frameworkTesting/cs311l-frameworkTestingTests/cs311l_frameworkTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  cs311l_frameworkTestingTests.swift
+//  cs311l-frameworkTestingTests
+//
+//  Created by June Ryleigh on 10/23/24.
+//
+
+import Testing
+@testable import cs311l_frameworkTesting
+
+struct cs311l_frameworkTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ cs311l-frameworkTesting/cs311l-frameworkTestingUITests/cs311l_frameworkTestingUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  cs311l_frameworkTestingUITestsLaunchTests.swift
+//  cs311l-frameworkTestingUITests
+//
+//  Created by June Ryleigh on 10/23/24.
+//
+
+import XCTest
+
+final class cs311l_frameworkTestingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
